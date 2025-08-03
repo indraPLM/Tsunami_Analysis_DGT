9ffc76a7160b7a575d60d80426ca6df7384c284b
indraPLM <112387467+indraPLM@users.noreply.github.com>
2025-08-03 23:42:45 +0700

    Update 1_Sea_Level_Monitoring.py

--- 1_Sea_Level_Monitoring.py
+++ 1_Sea_Level_Monitoring.py
@@ -230,7 +230,7 @@ with tab2:
 
     eq_time = datetime.strptime("2025-07-29 23:24:52", "%Y-%m-%d %H:%M:%S")  # UTC
     zoom_start = eq_time - pd.Timedelta(hours=4*24)
-    zoom_end = eq_time + pd.Timedelta(hours=2*24)
+    zoom_end = eq_time + pd.Timedelta(hours=1*24)
 
     # Use the 15 closest DART stations from your DataFrame
     for _, row in df_dart_closest.sort_values("distance_km").head(15).iterrows():
